c2264bcb84d0c337825517676b34939d3357d879
danielblock <ntlord@gmail.com>
2016-11-05 18:58:59 -0400

    initial

--- index.php
+++ index.php
@@ -1,133 +1,98 @@
 <?php
-
-
+set_time_limit(0);
+ignore_user_abort(1);
 //SELECT *, MATCH(trialNumber,patentNumber,petitionerPartyName,patentOwnerName) AGAINST ('Arista') as Relevance FROM trials WHERE MATCH (trialNumber,patentNumber,petitionerPartyName,patentOwnerName) AGAINST('Arista' IN NATURAL LANGUAGE MODE) HAVING Relevance > .5 ORDER BY Relevance DESC
-function getTrialsJSON($offset){
-	$jsonurl = "https://ptabdata.uspto.gov/ptab-api/trials?limit=100&sort=-filingDate&offset=".$offset;
-    $ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL, $jsonurl);
-	$result = curl_exec($ch);
-	curl_close($ch);
 
-	$results = json_decode($result);
-    $results = $results->{'results'};
-    //var_dump($results);
-	return $results;
-}
 
-function getLastTrialNumberRetrieved(){
-    $db = new mysqli("localhost", "root", "root", "ptab");
+//define constants
+$TRIALNUMBER='trialNumber';
+$PATENTNUMBER='patentNumber';
+$PATENTOWNERNAME='patentOwnerName';
+$PETITIONERNAME='petitionerPartyName';
+
+//search the local database
+function searchDatabase($type,$searchTerm){
+    $db = new mysqli("localhost", "root", "root", "ptab"); //connect
 
     if($db->connect_errno > 0){
         die('Unable to connect to database [' . $db->connect_error . ']');
     }
-    $SQL="SELECT * FROM lastTrialNumber WHERE ID=1";
-    if(!$result = $db->query($SQL)){
+
+    //setup the statement
+    $pStmt ="";
+    switch ($type) {
+        case $GLOBALS['TRIALNUMBER']:
+            $pStmt= "SELECT * FROM trials WHERE trialNumber LIKE ? LIMIT 1000";
+            break;
+        case $GLOBALS['PATENTNUMBER']:
+            $pStmt= "SELECT * FROM trials WHERE patentNumber LIKE ? LIMIT 1000";
+            break;
+        case $GLOBALS['PATENTOWNERNAME']:
+            $pStmt= "SELECT * FROM trials WHERE patentOwnerName LIKE ? LIMIT 1000";
+            break;
+        case $GLOBALS['PETITIONERNAME']:
+            $pStmt ="SELECT * FROM trials WHERE petitionerPartyName LIKE ? LIMIT 1000";
+            break;
+    }
+
+    $stmt = $db->prepare($pStmt); //limited to 1000 just to stop shenanigans
+
+    $stmt->bind_param("s",$searchTerm); //bind the variables
+
+    if(!$stmt->execute()){
         die('There was an error running the getLastTrialNumber query [' . $db->error . ']');
         return true;
     }
-    if($result->num_rows==1){
-        $row = $result->fetch_assoc();
-        return $row['lastTrialNumber'];
+    $result=$stmt->get_result();
+    $myArray = array();
+    while($row = $result->fetch_object()) {
+        array_push($myArray, $row);
     }
 
+    $rowCount = $result->num_rows; //get the number of results returned
+
+    //create metadata array
+    $metadata =["limit"=>1000,"offset"=>0,"count"=>$rowCount,"links"=>["rel"=>"self","href"=>'https://ptabdata.uspto.gov/ptab-api/trials?'.$type."=".$searchTerm]];
+
+    //create results array, and add SQL results to it
+    $results  =["metadata"=>$metadata,"results"=> $myArray];
+
+    return $results;
 }
 
-function storeLastTrialNumberRetrieved($lastTrialNumber){
-    $db = new mysqli("localhost", "root", "root", "ptab");
-    if($db->connect_errno > 0){
-        die('Unable to connect to database [' . $db->connect_error . ']');
+//get the GET parameters
+$searchType=htmlspecialchars($_GET["searchType"]);
+$searchString=htmlspecialchars($_GET["searchString"]);
+$results = array();
+
+//limit searchstring length to >2 for database considerations and less than 100 to prevent shenanigans
+if($searchType !=null && $searchString!=null && strlen($searchString) < 100 && strlen($searchString) > 2) {
+    $searchString = "%".$searchString."%";
+    switch ($searchType) {
+        case $TRIALNUMBER:
+            $results = searchDatabase($TRIALNUMBER, $searchString);
+            break;
+        case $PATENTNUMBER:
+            $results = searchDatabase($PATENTNUMBER, $searchString);
+            break;
+        case $PATENTOWNERNAME:
+            $results = searchDatabase($PATENTOWNERNAME, $searchString);
+            break;
+        case $PETITIONERNAME:
+            $results = searchDatabase($PETITIONERNAME, $searchString);
+            break;
+        default:
+            print "INVALID SEARCH TYPE<br>";
+
     }
-    $SQL="UPDATE lastTrialNumber SET lastTrialNumber='".$lastTrialNumber."' WHERE ID='1'";
-
-    if(!$result = $db->query($SQL)){
-        die('There was an error running the storeLastTrialNumber query [' . $db->error . ']');
-        return true;
-    }
+}else{
+    print "INVALID SEARCH PARAMETERS";
 }
-
-function addTrialsToSQL($results,$lastTrial){
-    $resultCount = count($results);
-    if($resultCount>0) {
-        $db = new mysqli("localhost", "root", "root", "ptab");
-        if($db->connect_errno > 0){
-            die('Unable to connect to database [' . $db->connect_error . ']');
-        }
-
-        for ($i = 0; $i < $resultCount; $i++) {
-            $trialNumber = $results[$i]->{'trialNumber'};
-            if ($trialNumber!=$lastTrial){
-                $SQL = "INSERT INTO trials VALUES(\"" .
-                    $results[$i]->{'inventorName'} . "\",\"" .
-                    $trialNumber . "\",\"" .
-                    $results[$i]->{'filingDate'} . "\",\"" .
-                    $results[$i]->{'patentOwnerName'} . "\",\"" .
-                    $results[$i]->{'patentNumber'} . "\",\"" .
-                    $results[$i]->{'applicationNumber'} . "\",\"" .
-                    $results[$i]->{'petitionerPartyName'} . "\",\"" .
-                    $results[$i]->{'prosecutionStatus'} . "\",\"" .
-                    $results[$i]->{'accordedFilingDate'} . "\",\"" .
-                    $results[$i]->{'institutionDecisionDate'} . "\",\"" .
-                    $results[$i]->{'lastModifiedDatetime'} . "\")";
-
-
-                //error_log($SQL);
-                if(!$result = $db->query($SQL)){
-                    die('There was an error running the addTrialsToSQL insert [' . $db->error . ']');
-                    return true;
-                }
-
-                echo "SQL Statement Execute: ". $SQL."<br>";
-            }else{
-                //we've matched the last trial, time to break out of the for loop
-                return true;
-            }
-        }
-    }
-}
+print '<pre>'.json_encode($results, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES).'</pre>';
 
 
 
 
-//echo htmlentities($row);
-$lastTrialNumber =getLastTrialNumberRetrieved();
-echo "Last Trial Number Added: " . $lastTrialNumber . "<br>";
-echo "Retrieving the most-recent 100 PTAB trials<br>";
-$results=getTrialsJSON(0);
-echo "Got " . count($results) . " result(s)<br>";
-
-$nextLastTrialNumber = $results[0]->{'trialNumber'}; //since this is sorted by date, the top result is the next last number to store
-echo "New Most Recent Trial Number: " . $nextLastTrialNumber ."<br>";
-if($lastTrialNumber != $nextLastTrialNumber){
-    storeLastTrialNumberRetrieved($nextLastTrialNumber);
-    echo "Storing The Last Trial Number in MYSQL: " . $nextLastTrialNumber . "<br>";
-}
-//echo var_dump($obj);
-
-
-$resultCount=count($results);
-$currentOffset = 0;
-$j=0;
-while($resultCount > 0) {
-    echo "Adding Results to SQL database <br>";
-    $result = addTrialsToSQL($results, $lastTrialNumber);
-    echo "Results added to SQL database <br>";
-    if($result){//if addTrialToSQL returns true, we hit the last IPR gotten
-        echo "We have hit the Last IPR petition<br>";
-        break;
-    }
-
-    $currentOffset+=$resultCount;
-    echo "Setting Offset to: " . $currentOffset . "<br>";
-    error_log("Offset: " .$currentOffset);
-    $results=getTrialsJSON($currentOffset);//get the next results at offset based on length of last recieved
-    $resultCount= count($results); //update the result count
-    echo "Got " . $resultCount . " result(s)<br>";
-
-    $j++;
-}
 //var_dump($obj);
 ?>
-Done
+
---
+++ updateFromAPI.php
@@ -0,0 +1,133 @@
+<?php
+
+
+//SELECT *, MATCH(trialNumber,patentNumber,petitionerPartyName,patentOwnerName) AGAINST ('Arista') as Relevance FROM trials WHERE MATCH (trialNumber,patentNumber,petitionerPartyName,patentOwnerName) AGAINST('Arista' IN NATURAL LANGUAGE MODE) HAVING Relevance > .5 ORDER BY Relevance DESC
+function getTrialsJSON($offset){
+	$jsonurl = "https://ptabdata.uspto.gov/ptab-api/trials?limit=100&sort=-filingDate&offset=".$offset;
+    $ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $jsonurl);
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	$results = json_decode($result);
+    $results = $results->{'results'};
+    //var_dump($results);
+	return $results;
+}
+
+function getLastTrialNumberRetrieved(){
+    $db = new mysqli("localhost", "root", "root", "ptab");
+
+    if($db->connect_errno > 0){
+        die('Unable to connect to database [' . $db->connect_error . ']');
+    }
+    $SQL="SELECT * FROM lastTrialNumber WHERE ID=1";
+    if(!$result = $db->query($SQL)){
+        die('There was an error running the getLastTrialNumber query [' . $db->error . ']');
+        return true;
+    }
+    if($result->num_rows==1){
+        $row = $result->fetch_assoc();
+        return $row['lastTrialNumber'];
+    }
+
+}
+
+function storeLastTrialNumberRetrieved($lastTrialNumber){
+    $db = new mysqli("localhost", "root", "root", "ptab");
+    if($db->connect_errno > 0){
+        die('Unable to connect to database [' . $db->connect_error . ']');
+    }
+    $SQL="UPDATE lastTrialNumber SET lastTrialNumber='".$lastTrialNumber."' WHERE ID='1'";
+
+    if(!$result = $db->query($SQL)){
+        die('There was an error running the storeLastTrialNumber query [' . $db->error . ']');
+        return true;
+    }
+}
+
+function addTrialsToSQL($results,$lastTrial){
+    $resultCount = count($results);
+    if($resultCount>0) {
+        $db = new mysqli("localhost", "root", "root", "ptab");
+        if($db->connect_errno > 0){
+            die('Unable to connect to database [' . $db->connect_error . ']');
+        }
+
+        for ($i = 0; $i < $resultCount; $i++) {
+            $trialNumber = $results[$i]->{'trialNumber'};
+            if ($trialNumber!=$lastTrial){
+                $SQL = "INSERT INTO trials VALUES(\"" .
+                    $results[$i]->{'inventorName'} . "\",\"" .
+                    $trialNumber . "\",\"" .
+                    $results[$i]->{'filingDate'} . "\",\"" .
+                    $results[$i]->{'patentOwnerName'} . "\",\"" .
+                    $results[$i]->{'patentNumber'} . "\",\"" .
+                    $results[$i]->{'applicationNumber'} . "\",\"" .
+                    $results[$i]->{'petitionerPartyName'} . "\",\"" .
+                    $results[$i]->{'prosecutionStatus'} . "\",\"" .
+                    $results[$i]->{'accordedFilingDate'} . "\",\"" .
+                    $results[$i]->{'institutionDecisionDate'} . "\",\"" .
+                    $results[$i]->{'lastModifiedDatetime'} . "\")";
+
+
+                //error_log($SQL);
+                if(!$result = $db->query($SQL)){
+                    die('There was an error running the addTrialsToSQL insert [' . $db->error . ']');
+                    return true;
+                }
+
+                echo "SQL Statement Execute: ". $SQL."<br>";
+            }else{
+                //we've matched the last trial, time to break out of the for loop
+                return true;
+            }
+        }
+    }
+}
+
+
+
+
+//echo htmlentities($row);
+$lastTrialNumber =getLastTrialNumberRetrieved();
+echo "Last Trial Number Added: " . $lastTrialNumber . "<br>";
+echo "Retrieving the most-recent 100 PTAB trials<br>";
+$results=getTrialsJSON(0);
+echo "Got " . count($results) . " result(s)<br>";
+
+$nextLastTrialNumber = $results[0]->{'trialNumber'}; //since this is sorted by date, the top result is the next last number to store
+echo "New Most Recent Trial Number: " . $nextLastTrialNumber ."<br>";
+if($lastTrialNumber != $nextLastTrialNumber){
+    storeLastTrialNumberRetrieved($nextLastTrialNumber);
+    echo "Storing The Last Trial Number in MYSQL: " . $nextLastTrialNumber . "<br>";
+}
+//echo var_dump($obj);
+
+
+$resultCount=count($results);
+$currentOffset = 0;
+$j=0;
+while($resultCount > 0) {
+    echo "Adding Results to SQL database <br>";
+    $result = addTrialsToSQL($results, $lastTrialNumber);
+    echo "Results added to SQL database <br>";
+    if($result){//if addTrialToSQL returns true, we hit the last IPR gotten
+        echo "We have hit the Last IPR petition<br>";
+        break;
+    }
+
+    $currentOffset+=$resultCount;
+    echo "Setting Offset to: " . $currentOffset . "<br>";
+    error_log("Offset: " .$currentOffset);
+    $results=getTrialsJSON($currentOffset);//get the next results at offset based on length of last recieved
+    $resultCount= count($results); //update the result count
+    echo "Got " . $resultCount . " result(s)<br>";
+
+    $j++;
+}
+//var_dump($obj);
+?>
+Done
